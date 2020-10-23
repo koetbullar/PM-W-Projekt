068958ae3a06ff076e1a95b3cb828d68c966bf93
koetbullar <72986526+koetbullar@users.noreply.github.com>
2020-10-23 09:00:36 +0200

    Add files via upload

---
+++ JavaAdventures.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class JavaAdventures {
+	public static void main(String [] args)  {
+		Scanner scanner = new Scanner (System.in);
+		String plazerName;
+		int playerChoice;
+
+		System.out.println("You're stranded on an island.");
+		System.out.println("Whats is your name?");
+		scanner.nextLine();
+
+	}
+}
+
+	
