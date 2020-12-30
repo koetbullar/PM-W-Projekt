ceb0dba5a8b090014c89b9dc1b972b834e55091c
Patryk Romanowicz <rom.patryk@gmail.com>
2020-12-30 13:19:04 +0100

    fix code style

--- Hangman.java
+++ Hangman.java
@@ -1,16 +1,17 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner ;
 import java.lang.Math;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 public class Hangman {
   public static void main(String[] args) throws FileNotFoundException {
        
     gameLoop(); 
   }
 
-  public static String word() throws FileNotFoundException {
+  public static String word( ) throws FileNotFoundException {
     int listIndex = 0;
     File file = new File("file.txt");
     Scanner sc = new Scanner(file);
@@ -23,19 +24,20 @@ public class Hangman {
     int rand = (int) (Math.random() * wordList.size());
     return wordList.get(rand);
   }
-   
+
   public static String createUnderline(String givenWord) {
     String underline = "";
-    for ( int i = 0; i < givenWord.length(); i++) {
+    for (int i = 0; i < givenWord.length(); i++) {
       underline += "*";
     }
     return underline;
   }
-  public static String createNewStatus (char userentry, String givenWord, String underline){
+  
+  public static String createNewStatus (char userentry, String givenWord, String underline) {
     char [] newUnderline = underline.toCharArray();
     String newStatus ;
-    for ( int i = 0; i < givenWord.length(); i++ ) {
-      if ( givenWord.charAt(i) == userentry) {
+    for (int i = 0; i < givenWord.length(); i++) {
+      if (givenWord.charAt(i) == userentry) {
         newUnderline[i] = userentry; 
         }
     }
@@ -45,24 +47,26 @@ public class Hangman {
 
   public static void gameLoop() throws FileNotFoundException {
     Scanner in = new Scanner(System.in);   
-    int MaxErrors = 5;
+    int maxErrors = 5;
     int failedAttempts = 0;
     String givenWord = word();
     String currentStatus = createUnderline(givenWord);
     
     System.out.println(createUnderline(givenWord));
-    while (givenWord.equals(currentStatus) == false && failedAttempts <= MaxErrors ) {
+    while (givenWord.equals(currentStatus) == false && failedAttempts <= maxErrors) {
       char userEntry = in.next().charAt(0);
-      currentStatus = createNewStatus(userEntry, givenWord,currentStatus);
+      currentStatus = createNewStatus(userEntry, givenWord, currentStatus);
       System.out.println(currentStatus);
       //Hier kommt der Hangman hin
-      if (givenWord.contains("" + userEntry) == false ) {
-          failedAttempts ++;
+      if (givenWord.contains("" + userEntry) == false) {
+        failedAttempts++;
       }
     }
-    if(givenWord.equals(currentStatus)) {
+    if (givenWord.equals(currentStatus)) {
       System.out.println("Congratulations");
         
-    } else System.out.println("Looser");
+    } else {
+        System.out.println("Looser");
+    }
   } 
 }
