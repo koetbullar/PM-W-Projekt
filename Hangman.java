ad3c795a0f817cc0f1d970c91b49ab0da597f225
patryk.r <72986526+koetbullar@users.noreply.github.com>
2020-12-30 15:14:51 +0100

    Words databank (#3)
    * add file.txt containing words, edit Hangman.java to load words randomly from file.txt, add Quelle.txt
    
    * fix code style
    
    * fix code style
    
    * edit README.md and Quelle.md
    
    * fix style Quelle.md

--- Hangman.java
+++ Hangman.java
@@ -1,74 +1,72 @@
-import java.util.Scanner ;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.lang.Math;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Hangman {
-   public static void main(String[] args) {
+  public static void main(String[] args) throws FileNotFoundException {
        
-       
+    gameLoop(); 
+  }
 
+  public static String word() throws FileNotFoundException {
+    int listIndex = 0;
+    File file = new File("file.txt");
+    Scanner sc = new Scanner(file);
+    List<String> wordList = new ArrayList<>();
+    while (sc.hasNextLine()) {
+      wordList.add(listIndex, sc.nextLine());
+      listIndex++;
+    }
+    System.out.println(wordList); // shows current words List, to be deleted later
+    int rand = (int) (Math.random() * wordList.size());
+    return wordList.get(rand);
+  }
 
-      gameLoop();
-     
-   }
-
-   public static String word(){
-     String wordList[] =  { "Hallo", "Biologie", "Informatik", "Mertcan", "Weihnachten"};
-     int rand = (int) (Math.random() * wordList.length - 1  );
-     return wordList[rand];
-     
-   }
-
-   public static String createUnderline(String givenWord) {
-     String underline = "";
-     for ( int i = 0; i < givenWord.length(); i++) {
-       underline += "*";
-     }
-     return underline;
-   }
-   public static String createNewStatus (char userentry, String givenWord, String underline){
+  public static String createUnderline(String givenWord) {
+    String underline = "";
+    for (int i = 0; i < givenWord.length(); i++) {
+      underline += "*";
+    }
+    return underline;
+  }
+  
+  public static String createNewStatus(char userentry, String givenWord, String underline) {
     char [] newUnderline = underline.toCharArray();
-    String newStatus ;
-    for ( int i = 0; i < givenWord.length(); i++ ) {
-         if ( givenWord.charAt(i) == userentry) {
-             newUnderline[i] = userentry;
-           
-              
-         }
-       }
-        newStatus = new String(newUnderline);
-       return newStatus;
+    String newStatus;
+    for (int i = 0; i < givenWord.length(); i++) {
+      if (givenWord.charAt(i) == userentry) {
+        newUnderline[i] = userentry; 
+      }
+    }
+    newStatus = new String(newUnderline);
+    return newStatus;
+  }
 
-   }
-
-   public static void gameLoop() {
-    Scanner in = new Scanner(System.in); 
-    
-   
-    int MaxErrors = 5;
+  public static void gameLoop() throws FileNotFoundException {
+    Scanner in = new Scanner(System.in);   
+    int maxErrors = 5;
     int failedAttempts = 0;
-       
-
-
     String givenWord = word();
     String currentStatus = createUnderline(givenWord);
     
     System.out.println(createUnderline(givenWord));
-    while (givenWord.equals(currentStatus) == false && failedAttempts <= MaxErrors ) {
+    while (givenWord.equals(currentStatus) == false && failedAttempts <= maxErrors) {
       char userEntry = in.next().charAt(0);
-      currentStatus = createNewStatus(userEntry, givenWord,currentStatus);
+      currentStatus = createNewStatus(userEntry, givenWord, currentStatus);
       System.out.println(currentStatus);
       //Hier kommt der Hangman hin
-      if (givenWord.contains("" + userEntry) == false ) {
-          failedAttempts ++;
+      if (givenWord.contains("" + userEntry) == false) {
+        failedAttempts++;
       }
     }
-    if(givenWord.equals(currentStatus)) {
-        System.out.println("Congratulations");
+    if (givenWord.equals(currentStatus)) {
+      System.out.println("Congratulations");
         
-    }else System.out.println("Looser");
-    
-       
-         
-       }
-       
-   
+    } else {
+      System.out.println("Looser");
+    }
+  } 
 }
---
+++ Quelle.md
@@ -0,0 +1,14 @@
+# Quellenangabe
+---
+**List, ArrayList**
+YouTube: https://www.youtube.com/watch?v=pTAda7qU4LY
+Internet: https://www.callicoder.com/java-arraylist/
+---
+**Einlesen aus einem .txt File**
+Internet:
+https://stackoverflow.com/questions/19023906/scanning-text-file-into-array-of-objects
+https://www.codegrepper.com/code-examples/java/java+read+integer+from+text+file+into+array+scanne
+---
+**Error Handling IO**
+Internet:
+https://stackoverflow.com/questions/19788989/error-unreported-exception-filenotfoundexception-must-be-caught-or-declared-to
