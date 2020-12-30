e2365b9f9e76a39e54b30525cbc7b37e05973b5b
Patryk Romanowicz <rom.patryk@gmail.com>
2020-12-30 13:04:58 +0100

    add file.txt containing words, edit Hangman.java to load words randomly from file.txt, add Quelle.txt

--- Hangman.java
+++ Hangman.java
@@ -1,53 +1,52 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner ;
 import java.lang.Math;
+import java.util.ArrayList;
+import java.util.List;
 public class Hangman {
-   public static void main(String[] args) {
+  public static void main(String[] args) throws FileNotFoundException {
        
-       
+    gameLoop(); 
+  }
 
-
-      gameLoop();
-     
-   }
-
-   public static String word(){
-     String wordList[] =  { "Hallo", "Biologie", "Informatik", "Mertcan", "Weihnachten"};
-     int rand = (int) (Math.random() * wordList.length - 1  );
-     return wordList[rand];
-     
-   }
-
-   public static String createUnderline(String givenWord) {
-     String underline = "";
-     for ( int i = 0; i < givenWord.length(); i++) {
-       underline += "*";
-     }
-     return underline;
-   }
-   public static String createNewStatus (char userentry, String givenWord, String underline){
+  public static String word() throws FileNotFoundException {
+    int listIndex = 0;
+    File file = new File("file.txt");
+    Scanner sc = new Scanner(file);
+    List<String> wordList = new ArrayList<>();
+    while (sc.hasNextLine()) {
+      wordList.add(listIndex, sc.nextLine());
+      listIndex++;
+    }
+    System.out.println(wordList); // shows current words List, to be deleted later
+    int rand = (int) (Math.random() * wordList.size());
+    return wordList.get(rand);
+  }
+   
+  public static String createUnderline(String givenWord) {
+    String underline = "";
+    for ( int i = 0; i < givenWord.length(); i++) {
+      underline += "*";
+    }
+    return underline;
+  }
+  public static String createNewStatus (char userentry, String givenWord, String underline){
     char [] newUnderline = underline.toCharArray();
     String newStatus ;
     for ( int i = 0; i < givenWord.length(); i++ ) {
-         if ( givenWord.charAt(i) == userentry) {
-             newUnderline[i] = userentry;
-           
-              
-         }
-       }
-        newStatus = new String(newUnderline);
-       return newStatus;
-
+      if ( givenWord.charAt(i) == userentry) {
+        newUnderline[i] = userentry; 
+        }
+    }
+    newStatus = new String(newUnderline);
+    return newStatus;
    }
 
-   public static void gameLoop() {
-    Scanner in = new Scanner(System.in); 
-    
-   
+  public static void gameLoop() throws FileNotFoundException {
+    Scanner in = new Scanner(System.in);   
     int MaxErrors = 5;
     int failedAttempts = 0;
-       
-
-
     String givenWord = word();
     String currentStatus = createUnderline(givenWord);
     
@@ -62,13 +61,8 @@ public class Hangman {
       }
     }
     if(givenWord.equals(currentStatus)) {
-        System.out.println("Congratulations");
+      System.out.println("Congratulations");
         
-    }else System.out.println("Looser");
-    
-       
-         
-       }
-       
-   
+    } else System.out.println("Looser");
+  } 
 }
